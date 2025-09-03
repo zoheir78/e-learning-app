567170dbfdda095aa6726154b739b7c29784a44d
“zoheir78” <zoulou178@gmail.com>
2025-09-03 17:15:19 +0100

    Fix MIDDLEWARE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,9 +65,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
