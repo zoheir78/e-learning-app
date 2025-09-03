befbf53fda5cb78a36399bfeb3aa2962c0784118
“zoheir78” <zoulou178@gmail.com>
2025-09-03 16:41:30 +0100

    Fix STATIC_URL in settings file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,7 +157,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
