fef7085bcc4e8520c4b72eec023433c30a8efad8
“zoheir78” <zoulou178@gmail.com>
2025-09-03 18:43:33 +0100

    Fix: vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/static/',  // ✅ This tells Vite to prefix all asset URLs with /static/
   plugins: [react()],
-})
+});
