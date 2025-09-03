96c988fc89af114a1c9562f1cc87f9263f58bc81
“zoheir78” <zoulou178@gmail.com>
2025-09-03 01:16:36 +0100

    Add code to deployment

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -14,6 +14,10 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+# 👉 Add this line: This fixes the AppRegistryNotReady error
+import django
+
+django.setup()
 
 application = ProtocolTypeRouter(
     {
